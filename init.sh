4af1caa26dabcbfba2921f29c2be13babbfe4ed6
Gustavo G. Ribeiro <ggrbill@gmail.com>
2019-02-26 21:31:39 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,5 @@ bash migrate.sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
 sudo apt-get install powerline-fonts
 pip install powerline-shell
