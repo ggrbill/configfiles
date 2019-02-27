e15ec3ce66a262f2ce88ef84e55db768c8e32040
Gustavo G. Ribeiro <ggrbill@gmail.com>
2019-02-26 21:03:11 -0300

    Update init.sh (powerline-shell installation)

--- init.sh
+++ init.sh
@@ -3,3 +3,7 @@ bash migrate.sh
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+sudo apt-get install powerline-fonts
+pip install powerline-shell
